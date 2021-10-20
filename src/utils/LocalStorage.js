8220a205efe90e190f4488b807fc4ccabc1c2b25
tejfaster <tejfaster55@gmail.com>
2021-10-20 19:49:01 +0530

    update

--- src/utils/LocalStorage.js
+++ src/utils/LocalStorage.js
@@ -1,6 +1,5 @@
-// import React from 'react'
 // import AsyncStorage from '@react-native-async-storage/async-storage';
-// import { useSelector } from 'react-redux';
+
 
 // export const storeLocalData = async (setType, data) => {
 
