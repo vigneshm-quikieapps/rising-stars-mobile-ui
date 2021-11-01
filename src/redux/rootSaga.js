7988b2d5400712e789f796abb142afdcefe1a363
“arun <arunsaras48.2@gmail.com>
2021-11-01 11:27:00 +0530

    fix rootSaga

--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -1,21 +1,24 @@
-import { all } from "redux-saga/effects"
-import { watcherLoginSaga,watcherPostcode,watcherRegister } from "./sagas/auth"
-import { watcherClubSaga, watcherClassSaga, watcherSessionSaga } from "./sagas/enrol"
-import { watcherGetUserProgressSaga } from "./sagas/progSagas"
-import { watcherClubSaga, watcherClassSaga, watcherSessionSaga, watcherClubfinance } from "./sagas/enrol"
-import {watchMemberSaga} from './sagas/home'
+import {all} from 'redux-saga/effects';
+import {watcherLoginSaga, watcherPostcode, watcherRegister} from './sagas/auth';
+import {watcherGetUserProgressSaga} from './sagas/progSagas';
+import {
+  watcherClubSaga,
+  watcherClassSaga,
+  watcherSessionSaga,
+  watcherClubfinance,
+} from './sagas/enrol';
+import {watchMemberSaga} from './sagas/home';
 
 export default function* rootSaga() {
-    yield all([
-        watcherLoginSaga(),
-        watcherClubSaga(),
-        watcherClassSaga(),
-        watcherSessionSaga(),
-        watcherPostcode(),
-        watcherRegister(),
-        watcherGetUserProgressSaga(),
-        watcherClubfinance(),
-        watchMemberSaga()
-    ])
+  yield all([
+    watcherLoginSaga(),
+    watcherClubSaga(),
+    watcherClassSaga(),
+    watcherSessionSaga(),
+    watcherPostcode(),
+    watcherRegister(),
+    watcherGetUserProgressSaga(),
+    watcherClubfinance(),
+    watchMemberSaga(),
+  ]);
 }
-
